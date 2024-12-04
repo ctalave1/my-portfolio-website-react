01a050dfafdc5c229b3d5cb09402c01e76995811
Christopher Talavera <talavera.christopher@yahoo.com>
2024-12-04 18:11:25 -0500

    Added Jsoc comments to root level project components
    -Added JSDoc comments to main.tsx and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,27 @@ import { ThemeContext, theme } from './contexts/ThemeContext';
 import { NavBar } from './components/NavBar/NavBar';
 import { Home } from './sections/Home';
 import { About } from './sections/About';
+import { Experience } from './sections/Experience';
 import { Skills } from './sections/Skills';
 import { Projects } from './sections/Projects';
 import { Contact } from './sections/Contact';
 import { Footer } from './sections/Footer';
 import { Background } from './components/Background';
-import { Experience } from './sections/Experience';
 
+/**
+ * The root component that renders the entire application layout.
+ * 
+ * The `App` component serves as the container for the main sections of the site, including navigation, 
+ * section content, and background animations. It also provides the theme context to all child components 
+ * using the `ThemeContext.Provider`.
+ * 
+ * @function
+ * @example
+ * // Example usage of the App component:
+ * <App />
+ * 
+ * @returns {JSX.Element} The root structure of the app, including sections, navigation, and background.
+ */
 const App = () => {
   return (
     <ThemeContext.Provider value={theme}>
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,23 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+
+/**
+ * The root rendering of the React application.
+ * 
+ * This function initializes the React app by rendering the root component (`App`) inside
+ * the HTML element with the ID of `root`. It wraps the app in React's `StrictMode` to help
+ * identify potential issues in the app during development.
+ * 
+ * @function
+ * @example
+ * // The root element is initialized and the app is rendered in StrictMode
+ * createRoot(document.getElementById('root')!).render(
+ *   <StrictMode>
+ *     <App />
+ *   </StrictMode>,
+ * );
+ */
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
